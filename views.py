868fb705db6d70d0eac6c0c2916e49378d59cae2
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-05-19 16:02:29 -0300

    Arrumando o casa

---
+++ views.py
@@ -0,0 +1,101 @@
+import time
+from flask import render_template, request, redirect, session, flash, url_for, send_from_directory
+from models import Jogo
+from dao import JogoDao, UsuarioDao
+from helpers import deleta_arquivo, recupera_imagem
+from jogoteca import db, app
+
+jogo_dao = JogoDao(db)
+usuario_dao = UsuarioDao(db)
+
+@app.route('/')
+def index():
+    lista = jogo_dao.listar()
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('novo')))
+
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogo = jogo_dao.salvar(jogo)
+
+    arquivo = request.files['arquivo']
+    if arquivo:
+        upload_path = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+
+    return redirect(url_for('index'))
+
+@app.route('/editar/<int:id>')
+def editar(id):
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login', proxima=url_for('editar')))
+
+    jogo = jogo_dao.buscar_por_id(id)
+    nome_imagem = recupera_imagem(id)
+    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=nome_imagem)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    id = request.form['id']
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console, id=id)
+    jogo_dao.salvar(jogo)
+
+    arquivo = request.files['arquivo']
+    if arquivo:
+        upload_path = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        deleta_arquivo(jogo.id)
+        arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')
+    return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    jogo_dao.deletar(id)
+    flash('O jogo foi removido com sucesso!')
+    return redirect(url_for('index'))
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    usuario = usuario_dao.buscar_por_id(request.form['usuario'])
+    if usuario:
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logado com sucesso!')
+            proxima_pagina = request.form['proxima']
+
+            if proxima_pagina:
+                return redirect(proxima_pagina)
+
+            return redirect(url_for('index'))
+    
+    flash('Usuário não logado')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('index'))
+
+@app.route('/uploads/<nome_arquivo>')
+def imagem(nome_arquivo):
+    return send_from_directory('uploads', nome_arquivo)
